647aa063d2433ac338cf78b99f69fa7a53722135
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2022-04-14 17:18:19 -0400

    Update question2.py

--- src/question2.py
+++ src/question2.py
@@ -71,32 +71,32 @@ class LIF:
 		:param seed: The seed for the random number generator.
 		"""
 		np.random.seed(seed)
-		
+
 		self.n_exc = n_exc
 		self.n_inh = n_inh
 		self.exc_indexes = np.arange(n_exc)
 		self.inh_indexes = np.arange(n_exc, n_exc + n_inh)
 		self.N_indexes = np.arange(n_exc + n_inh)
-		
+
 		self.E_exc = v_thresh + 15 if E_exc is None else E_exc
 		self.E_inh = v_reset - 15 if E_inh is None else E_inh
 		self.E = np.zeros(n_exc + n_inh, dtype=np.float32)
 		self.E[self.exc_indexes] = self.E_exc
 		self.E[self.inh_indexes] = self.E_inh
-		
+
 		self.p_EE = p_EE
 		self.p_EI = p_EI
 		self.p_IE = p_IE
 		self.p_II = p_II
 		self.c = np.zeros((n_exc + n_inh, n_exc + n_inh), dtype=np.float32)
 		self.set_p(p_EE, p_EI, p_IE, p_II)
-		
+
 		self.v_thresh = v_thresh
 		self.v_reset = v_reset
 		self.g_L = g_L
 		self.E_L = E_L
 		self.tau_syn = tau_syn
-	
+
 	def set_p(self, p_EE: float = None, p_EI: float = None, p_IE: float = None, p_II: float = None):
 		"""
 		Set the connection probabilities.
@@ -114,7 +114,7 @@ class LIF:
 			self.p_IE = p_IE
 		if p_II is not None:
 			self.p_II = p_II
-		
+
 		self.c = np.zeros((self.n_exc + self.n_inh, self.n_exc + self.n_inh), dtype=np.float32)
 		self.c[np.ix_(self.exc_indexes, self.exc_indexes)] = (
 				np.random.rand(self.n_exc, self.n_exc) < self.p_EE
@@ -128,9 +128,9 @@ class LIF:
 		self.c[np.ix_(self.inh_indexes, self.inh_indexes)] = (
 				np.random.rand(self.n_inh, self.n_inh) < self.p_II
 		).astype(np.float32)
-		
+
 		np.fill_diagonal(self.c, 0.0)
-	
+
 	def get_neuron_type(self, n_idx: int):
 		"""
 		Get the type of the neuron with the given index.
@@ -143,7 +143,7 @@ class LIF:
 			return 'inh'
 		else:
 			raise ValueError(f'Unknown neuron index: {n_idx}')
-	
+
 	def get_neuron_indexes(self, n_type: str):
 		"""
 		Get the indexes of the neurons of the given type.
@@ -156,10 +156,10 @@ class LIF:
 			return self.inh_indexes
 		else:
 			raise ValueError(f'Unknown neuron type: {n_type}')
-	
+
 	def __call__(self, *args, **kwargs):
 		return self.run(*args, **kwargs)
-	
+
 	def run(
 			self,
 			I_exc_func: IFunc = IConst(0.0),
@@ -180,29 +180,29 @@ class LIF:
 		time_steps = int(T_ms / dt)
 		t_space = np.arange(start=0, stop=int(T_ms / dt), step=1, dtype=int)
 		t_space_ms = np.arange(start=0, stop=T_ms, step=dt, dtype=np.float32)
-		
+
 		v_trace = self.v_reset * np.ones((time_steps, *self.E.shape), dtype=np.float32)
 		g_E_trace = np.zeros((time_steps, *self.E.shape), dtype=np.float32)
 		g_I_trace = np.zeros((time_steps, *self.E.shape), dtype=np.float32)
 		spikes_trace = np.zeros((time_steps, *self.E.shape), dtype=np.float32)
-		
+
 		I_in_trace = np.zeros((time_steps, *self.E.shape), dtype=np.float32)
 		I_in_trace[:, self.exc_indexes] = np.expand_dims(I_exc_func(t_space_ms), axis=-1)
 		I_in_trace[:, self.inh_indexes] = np.expand_dims(I_inh_func(t_space_ms), axis=-1)
-		
+
 		for t in tqdm.tqdm(t_space[:-1], disable=not verbose):
 			I_syn = g_E_trace[t] * (self.E_exc - v_trace[t]) + g_I_trace[t] * (self.E_inh - v_trace[t])
 			dvdt = self.g_L * (self.E_L - v_trace[t]) + I_syn + I_in_trace[t]
 			v_trace[t + 1] = v_trace[t] + dt * dvdt
 			spikes_trace[t + 1] = np.where(v_trace[t + 1] >= self.v_thresh, 1.0, 0.0)
 			v_trace[t + 1] = np.where(np.isclose(spikes_trace[t + 1], 1.0), self.v_reset, v_trace[t + 1])
-			
+
 			g_E_syn = np.dot(self.c[np.ix_(self.N_indexes, self.exc_indexes)], spikes_trace[t][self.exc_indexes])
 			g_E_trace[t + 1] = g_E_trace[t] * (1 - dt / self.tau_syn) + g_E_syn
-			
+
 			g_I_syn = np.dot(self.c[np.ix_(self.N_indexes, self.inh_indexes)], spikes_trace[t][self.inh_indexes])
 			g_I_trace[t + 1] = g_I_trace[t] * (1 - dt / self.tau_syn) + g_I_syn
-		
+
 		return LIFOutput(
 			T_ms=T_ms,
 			dt=dt,
@@ -214,7 +214,7 @@ class LIF:
 			g_I=g_I_trace,
 			I=I_in_trace
 		)
-	
+
 	@staticmethod
 	def get_firing_rate(lif_output: LIFOutput, reduce: bool = False) -> Union[np.ndarray, float]:
 		"""
@@ -227,22 +227,40 @@ class LIF:
 		if reduce:
 			firing_rate = np.mean(firing_rate)
 		return firing_rate
-	
-	def show_c(self):
-		ax = sns.heatmap(self.c)
-		plt.show()
+
+	def show_c(self, show: bool = False, ax: plt.Axes = None, save: bool = False, savename: str = "c.png"):
+		ax = sns.heatmap(self.c, ax=ax)
+		ax.set_xlabel("Neuron j [-]", fontsize=22)
+		ax.set_ylabel("Neuron i [-]", fontsize=22)
+		if save:
+			plt.savefig(savename)
+		if show:
+			plt.show()
+		return ax
 
 
 def question_2_exploration(show=False):
 	from src.ifunc import IConst
-	
-	lif = LIF(n_exc=50, n_inh=50, p_EE=0.05, p_EI=0.0, p_IE=0.0, p_II=0.0)
-	lif.show_c()
-	lif_output = lif.run(IConst(5.0), IConst(0.0), T_ms=100, dt=0.025)
-	
-	n_neurone = 3
+
+	os.makedirs("figures", exist_ok=True)
+
+	lif = LIF(n_exc=50, n_inh=50, p_EE=0.1, p_EI=0.1, p_IE=0.05, p_II=0.05)
+	fig, ax = plt.subplots(figsize=(12, 8))
+	ax = lif.show_c(ax=ax)
+	fig.savefig("figures/q2a_explo_c.png", dpi=300)
+	plt.close(fig)
+	lif_output = lif.run(IConst(4.0), IConst(1.0), T_ms=20, dt=0.025)
+
+	n_neurone = 4
 	fr_mean = LIF.get_firing_rate(lif_output, reduce=True)
-	neuron_indexes = np.argsort(np.abs(fr_mean - LIF.get_firing_rate(lif_output, reduce=False)))[:n_neurone]
+	exc_neuron_indexes = lif.exc_indexes[np.argsort(
+		np.abs(fr_mean - LIF.get_firing_rate(lif_output, reduce=False)[lif.exc_indexes])
+	)[:n_neurone // 2]]
+	inh_neuron_indexes = lif.inh_indexes[np.argsort(
+		np.abs(fr_mean - LIF.get_firing_rate(lif_output, reduce=False)[lif.inh_indexes])
+	)[:n_neurone - exc_neuron_indexes.size]]
+	neuron_indexes = np.concatenate([exc_neuron_indexes, inh_neuron_indexes])
+
 	# neuron_indexes = np.argsort(LIF.get_firing_rate(lif_output, reduce=False))[-n_neurone:]
 	# neuron_indexes = [0, ]
 	fig, axes = plt.subplots(4, len(neuron_indexes), figsize=(8, 5), sharex='all', sharey='row')
@@ -253,21 +271,21 @@ def question_2_exploration(show=False):
 	for i, neuron_idx in enumerate(neuron_indexes):
 		axes[0][i].set_title(f"Neuron {lif.get_neuron_type(neuron_idx)} {neuron_idx}")
 		axes[0][i].plot(lif_output.t_space_ms, lif_output.v[:, neuron_idx])
+		# axes[0][i].plot(lif_output.t_space_ms, lif.v_thresh*np.ones_like(lif_output.t_space_ms), color='k')
 		axes[1][i].plot(lif_output.t_space_ms, lif_output.g_E[:, neuron_idx])
 		axes[2][i].plot(lif_output.t_space_ms, lif_output.g_I[:, neuron_idx])
 		axes[3][i].plot(lif_output.t_space_ms, lif_output.I[:, neuron_idx])
-	
+
 	axes[0][0].set_ylabel("V [mV]")
-	axes[1][0].set_ylabel("g_E [?]")
-	axes[2][0].set_ylabel("g_I [?]")
-	axes[3][0].set_ylabel("I [?]")
+	axes[1][0].set_ylabel("$g_E$ [$mS/cm^2$]")
+	axes[2][0].set_ylabel("$g_I$ [$mS/cm^2$]")
+	axes[3][0].set_ylabel(r"I [$\mu A/cm^2$]")
 	axes[-1][int(len(neuron_indexes) / 2)].set_xlabel("Time [ms]")
 	plt.tight_layout(pad=2.0)
-	os.makedirs("figures", exist_ok=True)
 	fig.savefig("figures/q2a_explo_traces.png", dpi=300)
 	if show:
 		plt.show()
-	
+
 	neuron_indexes = lif.exc_indexes
 	fig, ax = plt.subplots(1, 1, figsize=(14, 8))
 	pad = 0.5
@@ -295,94 +313,115 @@ def question_2a_worker(lif, p_EE, I_E_value):
 
 def question_2a(resolution: int = 25, show=False):
 	sns.set_theme()
-	p_EE_space = np.linspace(0.0, 0.5, num=resolution)
-	I_E_value_space = np.linspace(0.0, 20.0, num=resolution)
-	
-	pp, II = np.meshgrid(p_EE_space, I_E_value_space)
-	
-	# store all the combination of p_EE and I_E
-	params_space = np.asarray((pp, II)).T.reshape(-1, 2)
-	
-	lif = LIF(n_exc=50, n_inh=50, p_EE=0.0, p_EI=0.0, p_IE=0.0, p_II=0.0)
-	firing_rates_list = apply_func_multiprocess(
-		func=question_2a_worker,
-		iterable_of_args=[(lif, p_EE, I_E_value) for p_EE, I_E_value in params_space]
-	)
-	
-	fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-	ax = sns.heatmap(
-		np.flip(np.asarray(firing_rates_list).reshape((*p_EE_space.shape, *I_E_value_space.shape)).T, axis=0),
-		xticklabels=['{:,.2f}'.format(x) for x in p_EE_space],
-		yticklabels=['{:,.2f}'.format(x) for x in I_E_value_space[::-1]],
-		cbar_kws={'label': 'Mean spiking rate [Hz]'},
-		ax=ax,
-	)
-	ax.set_xlabel("$p_{EE}$ [-]")
-	ax.set_ylabel("$I_E$ [?]")
-	os.makedirs("figures", exist_ok=True)
-	plt.savefig("figures/q2a_heatmap.png", dpi=300)
-	if show:
-		plt.show()
+	p_EE_ranges = [
+		(0.1, 0.23),
+		(0.15, 0.19),
+	]
+	I_E_ranges = [
+		(0.0, 100.0),
+		(1.95, 2.5),
+	]
+	firing_rates_lists = []
+	for i, ((p_EE_min, p_EE_max), (I_E_min, I_E_max)) in enumerate(zip(p_EE_ranges, I_E_ranges)):
+		if i == 0:
+			continue
+		p_EE_space = np.linspace(p_EE_min, p_EE_max, num=resolution)
+		I_E_value_space = np.linspace(I_E_min, I_E_max, num=resolution)
+
+		pp, II = np.meshgrid(p_EE_space, I_E_value_space)
+
+		# store all the combination of p_EE and I_E
+		params_space = np.asarray((pp, II)).T.reshape(-1, 2)
+
+		lif = LIF(n_exc=50, n_inh=50, p_EE=0.0, p_EI=0.0, p_IE=0.0, p_II=0.0)
+		firing_rates_list = apply_func_multiprocess(
+			func=question_2a_worker,
+			iterable_of_args=[(lif, p_EE, I_E_value) for p_EE, I_E_value in params_space]
+		)
+
+		fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+		ax = sns.heatmap(
+			np.flip(np.asarray(firing_rates_list).reshape((*p_EE_space.shape, *I_E_value_space.shape)).T, axis=0),
+			xticklabels=['{:,.3f}'.format(x) for x in p_EE_space],
+			yticklabels=['{:,.3f}'.format(x) for x in I_E_value_space[::-1]],
+			cbar_kws={'label': 'Mean spiking rate [Hz]'},
+			ax=ax,
+		)
+		ax.set_xlabel("$p_{EE}$ [-]")
+		ax.set_ylabel(r"$I_E$ [$\mu A/cm^2$]")
+		os.makedirs("figures", exist_ok=True)
+		plt.savefig(f"figures/q2a_heatmap{i}.png", dpi=300)
+		if show:
+			plt.show()
+		firing_rates_lists.append(firing_rates_list)
+	return firing_rates_lists
 
 
 def question_2b_worker(lif, p_EE, I_E_value):
 	lif.set_p(p_EE=p_EE)
-	lif_output = lif.run(I_exc_func=IConst(I_E_value), T_ms=400, verbose=False)
+	lif_output = lif.run(I_exc_func=IConst(I_E_value), T_ms=100, verbose=False)
 	return lif_output
 
 
-def question_2b(resolution: int = 25, show=False):
-	# TODO: take the values from question_2b
-	# I_E_values = np.array([1.67, 2.50, 10.0, 18.0])
-	# p_EE_values = np.array([0.2, 0.15, 0.17, 0.17])
-	I_E_values = np.array([2.50, ])
-	p_EE_values = np.array([0.15, ])
+def question_2b(resolution: int = 25, show=False, I_E_values=None, p_EE_values=None):
+	# TODO: take the values from question_2a
+	# I_E_values = np.array([2.019, 2.019, 2.5, 2.431])
+	# p_EE_values = np.array([0.167, 0.17, 0.190, 0.185])
+	if I_E_values is None:
+		I_E_values = np.array([2.019, 2.019, 2.5])
+	if p_EE_values:
+		p_EE_values = np.array([0.167, 0.17, 0.190])
+	# I_E_values = np.array([2.50, ])
+	# p_EE_values = np.array([0.15, ])
 	assert I_E_values.size == p_EE_values.size
-	
+
 	lif = LIF(n_exc=50, n_inh=50, p_EE=0.0, p_EI=0.0, p_IE=0.0, p_II=0.0)
 	outputs = apply_func_multiprocess(
 		func=question_2b_worker,
 		iterable_of_args=[(lif, p_EE, I_E_value) for p_EE, I_E_value in zip(p_EE_values, I_E_values)]
 	)
-	
+
 	line_length = 1.0
 	pad = 0.5
-	
+
 	nrows = int(np.sqrt(I_E_values.size))
+	nrows = I_E_values.size
 	ncols = int(I_E_values.size / nrows)
-	fig, axes = plt.subplots(nrows, ncols, figsize=(12, 8), sharex='all')
+	fig, axes = plt.subplots(nrows, ncols, figsize=(14, 10), sharex='all')
 	axes = np.ravel([axes])
 	for i, (ax, out) in enumerate(zip(axes, outputs)):
-		for n_idx, spikes in enumerate(out.spikes.T_ms):
+		for n_idx, spikes in enumerate(out.spikes.T):
 			spikes_idx = out.t_space_ms[np.isclose(spikes, 1.0)]
 			ymin = (out.spikes.shape[-1] - n_idx) * (pad + line_length)
 			ax.vlines(spikes_idx, ymin=ymin, ymax=ymin + line_length, colors=[0, 0, 0])
-		
+
 		ax.set_xlabel("Time [ms]")
 		ax.get_yaxis().set_visible(False)
-	
+		ax.set_title(rf"$I_E = {I_E_values[i]:.3f}$ [$\mu A/cm^2$], " + r"$p_{EE}$" + rf"$ = {p_EE_values[i]:.3f}$ [-]")
+
+	plt.tight_layout(pad=2)
 	os.makedirs("figures", exist_ok=True)
-	plt.savefig("figures/q2b_raster_plots.png", dpi=300)
+	plt.savefig(f"figures/q2b_raster_plots.png", dpi=300)
 	if show:
 		plt.show()
 
 
 def question_2c_worker(lif, p_EI, p_IE):
 	lif.set_p(p_EE=0, p_II=0, p_EI=p_EI, p_IE=p_IE)
-	lif_output = lif.run(I_exc_func=IConst(8), T_ms=400, verbose=False)
+	lif_output = lif.run(I_exc_func=IConst(2.019), T_ms=400, verbose=False)
 	return LIF.get_firing_rate(lif_output, reduce=True)
 
 
 def question_2c(resolution: int = 25, show=False):
 	sns.set_theme()
 	np.random.seed(42)
-	
+
 	p_EI_space = np.linspace(0.0, 1.0, num=resolution)
 	p_IE_space = np.linspace(0.0, 1.0, num=resolution)
-	
+
 	pp_EI, pp_IE = np.meshgrid(p_EI_space, p_IE_space)
 	params_space = np.asarray((pp_EI, pp_IE)).T.reshape(-1, 2)
-	
+
 	lif = LIF(n_exc=50, n_inh=50, p_EE=0.0, p_EI=0.0, p_IE=0.0, p_II=0.0)
 	firing_rates_list = apply_func_multiprocess(
 		func=question_2c_worker,
@@ -404,8 +443,53 @@ def question_2c(resolution: int = 25, show=False):
 		plt.show()
 
 
+def question_2c_raster_worker(lif, p_EI, p_IE):
+	lif.set_p(p_EI=p_EI, p_IE=p_IE)
+	lif_output = lif.run(I_exc_func=IConst(2.019), T_ms=400, verbose=False)
+	return lif_output
+
+
+def question_2c_raster(resolution: int = 25, show=False):
+	p_EI_values = np.array([0.0, 0.21, 0.96, 0.62])
+	p_IE_values = np.array([0.17, 0.62, 0.96, 0.21])
+	assert p_EI_values.size == p_IE_values.size
+
+	lif = LIF(n_exc=50, n_inh=50, p_EE=0.0, p_EI=0.0, p_IE=0.0, p_II=0.0)
+	outputs = apply_func_multiprocess(
+		func=question_2c_raster_worker,
+		iterable_of_args=[(lif, p_EI, p_IE) for p_EI, p_IE in zip(p_EI_values, p_IE_values)]
+	)
+
+	line_length = 1.0
+	pad = 0.5
+
+	nrows = int(np.sqrt(p_EI_values.size))
+	nrows = p_EI_values.size
+	ncols = int(p_EI_values.size / nrows)
+	fig, axes = plt.subplots(nrows, ncols, figsize=(14, 10), sharex='all')
+	axes = np.ravel([axes])
+	for i, (ax, out) in enumerate(zip(axes, outputs)):
+		for n_idx, spikes in enumerate(out.spikes.T):
+			spikes_idx = out.t_space_ms[np.isclose(spikes, 1.0)]
+			ymin = (out.spikes.shape[-1] - n_idx) * (pad + line_length)
+			ax.vlines(spikes_idx, ymin=ymin, ymax=ymin + line_length, colors=[0, 0, 0])
+
+		ax.set_xlabel("Time [ms]")
+		ax.get_yaxis().set_visible(False)
+		ax.set_title(
+			r"$p_{EI}$" + rf"$ = {p_EI_values[i]:.2f}$ [-], " + r"$p_{IE}$" + rf"$ = {p_IE_values[i]:.2f}$ [-]"
+		)
+
+	plt.tight_layout(pad=2)
+	os.makedirs("figures", exist_ok=True)
+	plt.savefig(f"figures/q2c_raster_plots.png", dpi=300)
+	if show:
+		plt.show()
+
+
 if __name__ == '__main__':
-	# question_2_exploration(show=True)
-	question_2a(resolution=25, show=False)
-	# question_2b(show=True)
-	question_2c(resolution=25, show=False)
+	# question_2_exploration(show=False)
+	# question_2a(resolution=25, show=False)
+	# question_2b(show=False)
+	# question_2c(resolution=25, show=True)
+	question_2c_raster(resolution=25, show=False)
